3d5c4ea98db7c03fd77a8173175e7382401cfa04
Theresa Crabb <theresacrabb1@gmail.com>
2025-10-17 09:05:22 -0500

    Updated PostCSS Config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
-import tailwindcssPostcss from '@tailwindcss/postcss';
+import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: [tailwindcssPostcss(), autoprefixer()]
+  plugins: [tailwindcss(), autoprefixer()]
 }
