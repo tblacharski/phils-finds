6157c840fdac1d88552aedd9d075bd3ea69e95e2
Theresa Crabb <theresacrabb1@gmail.com>
2025-10-17 09:25:26 -0500

    fixed postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,12 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+import autoprefixer from 'autoprefixer';
+import postcssNested from 'postcss-nested';
+
+const config = {
   plugins: [
-    // A plugin that does not require configuration:
-    'simple-plugin-example',
- 
-    // A plugin which needs a configuration object:
-    [
-      'plugin-with-configuration',
-      {
-        optionA: '...',
-      },
-    ],
- 
-    // A plugin that is toggled on or off based on environment:
-    [
-      'plugin-toggled',
-      process.env.NODE_ENV === 'production'
-        ? {
-            optionA: '...',
-          }
-        : false,
-    ],
- 
-    // Boolean expressions are also valid.
-    // `true` enables the plugin, `false` disables the plugin:
-    ['plugin-toggled-2', true /* a === b, etc */],
-  ],
-}
+    autoprefixer,
+    postcssNested
+  ]
+};
+
+export default config;
