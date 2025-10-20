ae4be2e1db285932bd8aa4bf297763c5a69676c0
Theresa Crabb <theresacrabb1@gmail.com>
2025-10-20 14:44:58 -0500

    fixed all

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,9 @@ const nextConfig: NextConfig = {
       }
     ],
   },
+    experimental: {
+      useLightningcss: true,
+  },
 };
 
 export default nextConfig;
---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+// correct way in nextjs + tailwind v4
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
